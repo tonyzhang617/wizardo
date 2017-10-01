2c1de1e4a977567165e1fee6d30aed017de254da
tonyzhang617 <tonyzhang617@gmail.com>
2017-09-30 22:03:15 -0400

    Adds Round Screen initialization

--- app/screens/round.js
+++ app/screens/round.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 
 export class RoundScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasInit: false,
+    };
+  }
+
   static navigationOptions = ({navigation}) => ({
     title: `Round ${navigation.state.params.roundNum}`,
   });
+
+  componentWillMount() {
+    const { state, setParams } = this.props.navigation;
+
+    var newPlayers = state.params.players;
+    var dealer = newPlayers.shift();
+    newPlayers.push(dealer);
+
+    setParams({
+      players: newPlayers
+    });
+
+    console.log(`Round ${state.params.roundNum} initialized`);
+  }
+
   render() {
-
-    const { params } = this.props.navigation.state;
-
-    console.log(params.scores);
+    const { state, setParams } = this.props.navigation;
 
     return (
       <View>
